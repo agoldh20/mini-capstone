3f1fbd4857412dfc9e8906d66443b325c44568eb
agoldwater <dmgldwtr@gmail.com>
2017-09-05 21:30:33 -0500

    updated links and buttons to add to database

--- app/controllers/drones_controller.rb
+++ app/controllers/drones_controller.rb
@@ -12,10 +12,37 @@ class DronesController < ApplicationController
                       description: params[:description],
                       price: params[:price])
     drone.save
+    flash[:success] = "Drone Successfully Created"
+    redirect_to "/drones/#{drone.id}"
   end
 
   def show
     @drone = Drone.find(params[:id])
+  end
+
+  def edit
+    @drone = Drone.find(params[:id])
+  end
+
+  def update
+    drone = Drone.find(params[:id])
+
+    drone.assign_attributes(
+                      name: params[:name],
+                      description: params[:description],
+                      price: params[:price]
+                      )
+
+    drone.save
+    flash[:success] = "Drone Successfully Updated"
+    redirect_to "/drones/#{drone.id}"
+  end
+
+  def destroy
+    drone = Drone.find(params[:id])
+    drone.destroy
+    flash[:danger] = "Drone #{drone.name} Successfully Killed, MUHAHAHAHAHAHAHA"
+    redirect_to "/"
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,9 @@ Rails.application.routes.draw do
   get '/drones' => 'drones#index'
   get '/drones/:id' => 'drones#show'
 
+  get '/drones/:id/edit' => 'drones#edit'
+  patch '/drones/:id' => 'drones#update'
+
+  delete '/drones/:id' => 'drones#destroy'
 
 end
