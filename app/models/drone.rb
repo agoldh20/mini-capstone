971926ebe0d8c0706062e290894bc7d604a7781b
agoldwater <dmgldwtr@gmail.com>
2017-09-06 21:39:51 -0500

    added tax and methods

--- app/models/drone.rb
+++ app/models/drone.rb
@@ -1,2 +1,14 @@
 class Drone < ApplicationRecord
+  def sale_message
+    return "Discounted Item!" if price < 2
+    return "Everyday Value!" if price > 2
+  end
+
+  def tax
+    tax = price * 0.09
+  end
+
+  def total_price
+    total_price = price + tax
+  end
 end
