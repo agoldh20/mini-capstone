018d0a4b44830ce803b25185df92d2d2d192829f
agoldwater <dmgldwtr@gmail.com>
2017-10-29 12:00:49 -0500

    removed description validations

--- app/models/drone.rb
+++ app/models/drone.rb
@@ -9,7 +9,7 @@ class Drone < ApplicationRecord
   validates :name, uniqueness: true
   validates :price, presence: true
   validates :price, numericality: true
-  validates :description, length: {in: 200..500}
+  # validates :description, length: {in: 200..500}
   
   def sale_message
    return "Discounted Item!" if price <= 40
